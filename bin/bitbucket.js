6ea577612111e4156851312071c25bcd4daf1602
John West <john.west@adp.com>
2018-06-06 12:26:18 -0400

    build

--- bin/bitbucket.js
+++ bin/bitbucket.js
@@ -91,7 +91,6 @@ class BitBucket {
             }
             else if (composite.mergeRequested !== existingCachedComposite.mergeRequested ||
                 composite.title !== existingCachedComposite.title ||
-                composite.updatedDate.toISOString() !== existingCachedComposite.updatedDate.toISOString() ||
                 composite.approvals !== existingCachedComposite.approvals ||
                 composite.needWorks !== existingCachedComposite.needWorks ||
                 composite.openTasks !== existingCachedComposite.openTasks ||
