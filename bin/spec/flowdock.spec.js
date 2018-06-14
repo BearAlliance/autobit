0ab45886caab46b7247bb8486823b4993a0e41f1
John West <john.west@adp.com>
2018-06-14 12:12:54 -0400

    build

--- bin/spec/flowdock.spec.js
+++ bin/spec/flowdock.spec.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const flowdock_1 = require("../src/flowdock");
+const ts_mocks_1 = require("ts-mocks");
 describe('first', () => {
-    it('should create instance', () => {
-        let fd = new flowdock_1.Flowdock('foo', 'bar', 'baz');
-        expect(fd).toBeDefined();
+    let fd;
+    beforeEach(() => {
+        fd = new flowdock_1.Flowdock('foo', 'bar', 'baz');
+        fd.http = new ts_mocks_1.Mock().Object;
     });
 });
 //# sourceMappingURL=flowdock.spec.js.map
--- bin/src/main.js
+++ bin/src/main.js
@@ -22,14 +22,15 @@ const optionDefinitions = [
     { name: 'intervalSeconds', type: Number, defaultValue: 10 },
     { name: 'flowdockUsername', type: String, defaultValue: 'autobit' },
     { name: 'flowName', type: String },
-    { name: 'repository', type: String }
+    { name: 'repository', type: String },
+    { name: 'password', alias: 'p', type: String }
 ];
 const commandLineArgs = require('command-line-args');
 const options = commandLineArgs(optionDefinitions);
 class Main {
     start() {
         Promise.resolve(options.username || prompt('Enter your username: ')).then((username) => {
-            prompt('Enter your password: ', { method: 'hide' }).then((password) => {
+            prompt(options.password || 'Enter your password: ', { method: 'hide' }).then((password) => {
                 let fd = new flowdock_1.Flowdock(options.flowdockToken, options.flowdockUsername, options.flowName);
                 fd.initializeFlowdock().then(() => __awaiter(this, void 0, void 0, function* () {
                     let bb = new bitbucket_1.BitBucket(username, password, options.branch, options.repository, options.bitbucketBaseUrl, options.proxyBypass, options.proxyUrl, fd);
