4569803e6107be0318d034ef904f9c4a4c37e967
Nick Cacace <nick.cacace@adp.com>
2019-03-06 16:20:29 -0500

    chore: ignore bin in scm, only publish bin dir

--- bin/src/types/flowDockUser.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class flowDockUser {
-}
-exports.flowDockUser = flowDockUser;
-//# sourceMappingURL=flowDockUser.js.map
--- bin/src/types/merge.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MergeStatus {
-}
-exports.MergeStatus = MergeStatus;
-//# sourceMappingURL=merge.js.map
